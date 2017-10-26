1f5b0b72cbe1c3d21ce622461635217fa41516d8
Dima Maiboroda <dimayboroda@gmail.com>
2017-10-26 23:53:12 +0300

    first adaptive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
 
   link         = $('.menu-open');
-  link__active = $('.menu-open--active');
-  openMenu     = $('.open-nav')
+  openNav     = $('.open-nav')
   nav_link     = $('.open-nav a')
   link.click(function() {
     link.toggleClass('menu-open--active');
-    openMenu.toggleClass('open-nav--active');
+    openNav.toggleClass('open-nav--active');
   });
   nav_link.click(function() {
     link.toggleClass('menu-open--active');
-    openMenu.toggleClass('open-nav--active');
+    openNav.toggleClass('open-nav--active');
   });
   
+  $(".main-menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 500);
+  });
+
 });
