4819f43be6e74d71b2ac2cfec590920860f01d70
Dima Maiboroda <dimayboroda@gmail.com>
2018-03-19 21:03:26 +0200

    slider responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,6 +25,22 @@ $(document).ready(function() {
     slidesToScroll: 1,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 820,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
 });
